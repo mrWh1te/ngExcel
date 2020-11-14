185d92276b3056b06fdb32f1a357e3aea32f9256
Michael Lage <lage.michael@gmail.com>
2020-11-14 13:55:28 -0700

    added service worker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,17 @@ import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { LayoutsModule } from './layouts/layouts.module'
 import { CoreModule } from './core/core.module'
-import { SpreadsheetViewsModule } from './spreadsheet/spreadsheet-views/spreadsheet-views.module'
+import { SpreadsheetViewsModule } from './spreadsheet/spreadsheet-views/spreadsheet-views.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment'
 
 @NgModule({
   imports: [
     CoreModule.forRoot(),
     AppRoutingModule,
     LayoutsModule,
-    SpreadsheetViewsModule
+    SpreadsheetViewsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   declarations: [ AppComponent ],
   bootstrap: [ AppComponent ]
